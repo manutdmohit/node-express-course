ba41f272db0045cc2c2bccebf95ed1972ad8d3bb
manutdmohit <saudmohit@gmail.com>
2022-02-14 14:24:16 +0545

    All Routes

--- Task Manger/controllers/tasks.js
+++ Task Manger/controllers/tasks.js
@@ -1,3 +1,19 @@
 exports.getAllTasks = (req, res) => {
-  res.send('all times');
+  res.send('get all tasks');
+};
+
+exports.createTask = (req, res) => {
+  res.send('create task');
+};
+
+exports.getTask = (req, res) => {
+  res.send('get single tasks');
+};
+
+exports.updateTask = (req, res) => {
+  res.send('update task');
+};
+
+exports.deleteTask = (req, res) => {
+  res.send('delete task');
 };
