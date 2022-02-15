fcd31cb24f633b4544ca5eb44338ab78cc52bee8
manutdmohit <saudmohit@gmail.com>
2022-02-15 15:53:00 +0545

    delete task

--- Task Manger/controllers/tasks.js
+++ Task Manger/controllers/tasks.js
@@ -45,6 +45,21 @@ exports.updateTask = (req, res) => {
   res.json({ id: req.params.id });
 };
 
-exports.deleteTask = (req, res) => {
-  res.json({ id: req.params.id });
+exports.deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ message: `No task with id : ${taskID}` });
+    }
+
+    res.status(200).json({
+      message: task.id,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
 };
